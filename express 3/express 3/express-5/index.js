ee05ea4eacdc31b4bf2280e7b0765e1e2d1854c1
VaruniRai <varuni.25b01010316@abes.ac.in>
2026-09-08 11:42:30 +0530

    project7

---
+++ express 3/express 3/express-5/index.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Hello from Express 5!')); app.listen(3000, () => console.log('Server running at http://localhost:3000'));
